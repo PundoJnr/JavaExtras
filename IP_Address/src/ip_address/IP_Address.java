fba33cb19d70cb43bf60e3d14dc878a5419bcb99
PundoJnr <pundoarthur@gmail.com>
2017-07-18 12:58:38 -0700

    initial commit

---
+++ IP_Address/src/ip_address/IP_Address.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ip_address;
+import java.net.InetAddress;
+/**
+ *
+ * @author Pundo
+ */
+public class IP_Address {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception{
+       System.out.println(InetAddress.getLocalHost()); // TODO code application logic here
+    }
+    
+}
